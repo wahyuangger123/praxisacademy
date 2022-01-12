f79dfa50ef3334cfdd569bb1bae63771130bd5af
wahyuangger123 <anggerborneco@gmail.com>
2022-01-12 17:25:25 +0700

    hari pertama

---
+++ kemampuandasaracademy/novice/01.01/main.js
@@ -0,0 +1,55 @@
+var siteName="helloword";
+var angkaSaya=1234;
+siteName="hellojogja"
+angkaSaya=5678
+console.log(siteName,angkaSaya);
+
+//tipe data string
+const barangDijual="motorhonda";
+const hargaBarang=1500;
+console.log(hargaBarang,barangDijual);
+
+console.log("variabelstring&intergen............................");
+
+//tipe data number&operator
+const a = 9;
+const b = 2;
+const c = a+b 
+const d = a-b
+const e = a*b
+const f = a**b
+const g = a/b
+const h = a%b
+console.log(c,d,e,f,g,h);
+
+const lebihkecil = e < a;
+console.log(lebihkecil);
+const lebihbesar = a > d ;
+console.log(lebihbesar);
+
+// type data langsung
+console.log(a > b);
+console.log(b < c);
+console.log('rumah' != 'jalan');
+console.log('car' == 'mobil');
+
+// pertanyaan
+
+const namaKades = 'Sugiono';
+const namaCamat = 'Agus';
+const jabatan ='KADES'
+
+if (jabatan == 'KADES') {
+    console.log(namaCamat);
+} else {
+    console.log(namaCamat);
+}
+
+const apakahBoss = true;
+const apakahMiniBoss = false;
+
+if (apakahBoss && apakahMiniBoss) {
+    console.log('NaikLevel');
+} else 
+{ console.log('MasihCupu');
+}
